1317baf16fc6d48bee2e2ac3917914bde448436e
Alvaro <49353987+Zekess@users.noreply.github.com>
2022-05-05 01:41:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -120,6 +120,9 @@ elif pag_navegacion_actual == paginas_navegacion[3]:
     
     st.subheader(u'A\u2081')
     st.text(u'El índice A\u2081')
+    """ 
+    ASD 
+    """
     
     st.subheader('CVCL')
     
